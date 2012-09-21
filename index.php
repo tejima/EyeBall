fc94be0c7ab33dbd6ffd1376b96433b34c709057
手嶋守 <tejima@gmail.com>
2012-09-22 02:31:19 +0900

    list colmn.

--- index.php
+++ index.php
@@ -6,8 +6,17 @@ require_once "Gdata/ClientLogin.php";
 $service = Zend_Gdata_Spreadsheets::AUTH_SERVICE_NAME;
 $client = Zend_Gdata_ClientLogin::getHttpClient($_ENV['ID'], $_ENV['PASSWORD'], $service);
 $spreadsheetService = new Zend_Gdata_Spreadsheets($client);
-$feed = $spreadsheetService->getSpreadsheetFeed();
 
-print_r($feed);
+$query = new Zend_Gdata_Spreadsheets_ListQuery();
+$query->setSpreadSheetKey("0Ao32NvF7NnotdHJlZWFDX2NyZVVQZEZLcWdqR2I4NUE");
+$query->setWorksheetId(0);
+
+$listFeed = $spreadsheetService->getListFeed();
+
+$rowData = $listFeed->entries[1]->getCustom();
+
+foreach($rowData as $column){
+  echo $column->getColumnName() . " = " . $column->getText() . "\n";
+}
 
 echo "DONE";
