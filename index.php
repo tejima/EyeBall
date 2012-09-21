cca44fa1ff3a0e06aa323a298cf502c5b200f7a5
手嶋守 <tejima@gmail.com>
2012-09-22 03:20:32 +0900

    now() functionable?

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ $query = new Zend_Gdata_Spreadsheets_ListQuery();
 $query->setSpreadSheetKey("0Ao32NvF7NnotdHJlZWFDX2NyZVVQZEZLcWdqR2I4NUE");
 $query->setWorksheetId("od6");
 
-$query->setSpreadsheetQuery('NAVI=ACTIVE and TIME<9:51');
+$query->setSpreadsheetQuery("NAVI=ACTIVE and TIME <= now()");
 
 $listFeed = $spreadsheetService->getListFeed($query);
 
