1fe57e3ed5312bb213af9fbbf4a9345589728d1a
手嶋守 <tejima@gmail.com>
2012-09-22 02:05:05 +0900

    fix $_

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require_once "Zend/Gdata/Spreadsheets.php";
 
 $service = Zend_Gdata_Spreadsheets::AUTH_SERVICE_NAME;
-$client = Zend_Gdata_ClientLogin::getHttpClient(ENV['ID'], ENV['PASSWORD'], $service);
+$client = Zend_Gdata_ClientLogin::getHttpClient($_ENV['ID'], $_ENV['PASSWORD'], $service);
 $spreadsheetService = new Zend_Gdata_Spreadsheets($client);
 $feed = $spreadsheetService->getSpreadsheetFeed();
 
