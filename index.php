72e37f20368fba38d4a57a7dbd8d91506e851aab
手嶋守 <tejima@gmail.com>
2012-09-22 01:59:42 +0900

    id/pass data from ENV.

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 
 require_once "Zend/Gdata/Spreadsheets.php";
 
+$service = Zend_Gdata_Spreadsheets::AUTH_SERVICE_NAME;
+$client = Zend_Gdata_ClientLogin::getHttpClient(ENV['ID'], ENV['PASSWORD'], $service);
+$spreadsheetService = new Zend_Gdata_Spreadsheets($client);
+$feed = $spreadsheetService->getSpreadsheetFeed();
 
+print_r($feed);
 
 echo "DONE";
