10a6ca9a98c40f94a85232783a2ae50825a34e4f
手嶋守 <tejima@gmail.com>
2012-09-22 03:44:23 +0900

    index.php is under test. add index_beta .

---
+++ index_beta.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once "Gdata/Spreadsheets.php";
+require_once "Gdata/ClientLogin.php";
+
+$service = Zend_Gdata_Spreadsheets::AUTH_SERVICE_NAME;
+$client = Zend_Gdata_ClientLogin::getHttpClient($_ENV['ID'], $_ENV['PASSWORD'], $service);
+$spreadsheetService = new Zend_Gdata_Spreadsheets($client);
+
+/*
+
+$query = new Zend_Gdata_Spreadsheets_DocumentQuery();
+$query->setSpreadSheetKey("0Ao32NvF7NnotdHJlZWFDX2NyZVVQZEZLcWdqR2I4NUE");
+$feed = $spreadsheetService->getWorksheetFeed($query);
+
+print_r($feed);
+exit;
+*/
+
+$query = new Zend_Gdata_Spreadsheets_ListQuery();
+$query->setSpreadSheetKey("0Ao32NvF7NnotdHJlZWFDX2NyZVVQZEZLcWdqR2I4NUE");
+$query->setWorksheetId("od6");
+
+$query->setSpreadsheetQuery($_ENV["QUERY"]);
+
+$listFeed = $spreadsheetService->getListFeed($query);
+
+$rowData = $listFeed->entries[0]->getCustom();
+
+$result = array();
+foreach($rowData as $column){
+  if($column->getColumnName() == "message"){
+    $result["message"] = $column->getText();
+  }
+}
+
+$result["status"] = "success";
+
+
+$spreadsheetService->updateRow($listFeed->entries[0],array("navi" => "DONE"));
+
+echo json_encode($result);
