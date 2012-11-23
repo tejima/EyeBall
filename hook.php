0417e7fbc01e7c53a718a3bab9096fae2df4ce6d
手嶋守 <tejima@gmail.com>
2012-11-24 01:06:51 +0900

    add hook receiver

---
+++ hook.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once "Gdata/Spreadsheets.php";
+require_once "Gdata/ClientLogin.php";
+
+mail( "tejicubetejicube@ezweb.ne.jp" , "HOOK" , "HOOK BODY" ,"From: tejima@tejimaya.com"  );
