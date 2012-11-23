561f98c87c286dbbf7a54dd446dae87b9ace9d4b
手嶋守 <tejima@gmail.com>
2012-11-24 01:10:36 +0900

    result info

--- hook.php
+++ hook.php
@@ -1,6 +1,9 @@
 <?php
-
 require_once "Gdata/Spreadsheets.php";
 require_once "Gdata/ClientLogin.php";
+$result = mail( "tejicubetejicube@ezweb.ne.jp" , "HOOK" , "HOOK BODY" ,"From: tejima@tejimaya.com");
 
-mail( "tejicubetejicube@ezweb.ne.jp" , "HOOK" , "HOOK BODY" ,"From: tejima@tejimaya.com"  );
+
+echo "TEST";
+
+echo "RESULT: $result";
