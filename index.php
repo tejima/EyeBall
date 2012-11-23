9184238d01ab1470ad2f76ef2ab9a610cbe66776
手嶋守 <tejima@gmail.com>
2012-11-24 01:19:40 +0900

    hook log to google spreadsheets

--- index.php
+++ index.php
@@ -3,39 +3,11 @@
 require_once "Gdata/Spreadsheets.php";
 require_once "Gdata/ClientLogin.php";
 
+$key = "0Ao32NvF7NnotdHJlZWFDX2NyZVVQZEZLcWdqR2I4NUE";
+$sheetid = "0Ao32NvF7NnotdHJlZWFDX2NyZVVQZEZLcWdqR2I4NUE";
 $service = Zend_Gdata_Spreadsheets::AUTH_SERVICE_NAME;
 $client = Zend_Gdata_ClientLogin::getHttpClient($_ENV['ID'], $_ENV['PASSWORD'], $service);
 $spreadsheetService = new Zend_Gdata_Spreadsheets($client);
 
-/*
-
-$query = new Zend_Gdata_Spreadsheets_DocumentQuery();
-$query->setSpreadSheetKey("0Ao32NvF7NnotdHJlZWFDX2NyZVVQZEZLcWdqR2I4NUE");
-$feed = $spreadsheetService->getWorksheetFeed($query);
-
-print_r($feed);
-exit;
-*/
-
-$query = new Zend_Gdata_Spreadsheets_ListQuery();
-$query->setSpreadSheetKey("0Ao32NvF7NnotdHJlZWFDX2NyZVVQZEZLcWdqR2I4NUE");
-$query->setWorksheetId("od6");
-
-$query->setSpreadsheetQuery($_ENV["QUERY"]);
-
-$listFeed = $spreadsheetService->getListFeed($query);
-
-$rowData = $listFeed->entries[0]->getCustom();
-
-$result = array();
-foreach($rowData as $column){
-  if($column->getColumnName() == "message"){
-    $result["message"] = $column->getText();
-  }
-}
-
-$result["status"] = "success";
-echo json_encode($result);
-
-$spreadsheetService->deleteRow($listFeed->entries[0]);
+$insertedListEntry = $spreadsheetService->insertRow(array("message"=>"hook"), $key, $sheetid);
 
