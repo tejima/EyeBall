1c2532a3d438ed510d3c35b9de38bd5a8dac3f72
手嶋守 <tejima@gmail.com>
2012-11-24 04:32:32 +0900

    catch error

--- index.php
+++ index.php
@@ -7,45 +7,51 @@ $sheetkey = "0Ao32NvF7NnotdHJlZWFDX2NyZVVQZEZLcWdqR2I4NUE";
 $sheetid = "od6";
 $service = Zend_Gdata_Spreadsheets::AUTH_SERVICE_NAME;
 
-
-$id = $_SERVER['ID'] ? $_SERVER['ID'] : $_ENV['ID'];
-$password = $_SERVER['PASSWORD'] ? $_SERVER['PASSWORD'] : $_ENV['ID'];
-$query_string = $_SERVER['QUERY'] ? $_SERVER['QUERY'] : $_SERVER['QUERY'];
-$client = Zend_Gdata_ClientLogin::getHttpClient($id , $password, $service);
-$spreadsheetService = new Zend_Gdata_Spreadsheets($client);
-
-$query = new Zend_Gdata_Spreadsheets_ListQuery();
-$query->setSpreadSheetKey($sheetkey);
-$query->setWorksheetId($sheetid);
-
-$query->setSpreadsheetQuery($query_string);
-$listFeed = $spreadsheetService->getListFeed($query);
-$rowData = $listFeed->entries[0]->getCustom();
-
-$result = array();
-
-$data = $listFeed->entries[0]->getCustomByName("message");
+try{
 
 
-foreach ($listFeed->entries as $line) {
-  $status = $line->getCustomByName("status");
-  if($status == DONE){
-  	//skip
-  	continue;
-  }
-  $result["message"] = (string)$line->getCustomByName("message");
-  $result["theme"] = (string)$line->getCustomByName("theme");
-  $result["status"] = "success";
+	$id = $_SERVER['ID'] ? $_SERVER['ID'] : $_ENV['ID'];
+	$password = $_SERVER['PASSWORD'] ? $_SERVER['PASSWORD'] : $_ENV['ID'];
+	$query_string = $_SERVER['QUERY'] ? $_SERVER['QUERY'] : $_SERVER['QUERY'];
+	$client = Zend_Gdata_ClientLogin::getHttpClient($id , $password, $service);
+	$spreadsheetService = new Zend_Gdata_Spreadsheets($client);
 
-  echo json_encode($result);
-  //$newline = $line->getCustom();
-  $newline["status"] = "DONE";
-  //$spreadsheetService->deleteRow($listFeed->entries[0]);
-  $spreadsheetService->updateRow($line,$newline);
-  exit();
+	$query = new Zend_Gdata_Spreadsheets_ListQuery();
+	$query->setSpreadSheetKey($sheetkey);
+	$query->setWorksheetId($sheetid);
+
+	$query->setSpreadsheetQuery($query_string);
+	$listFeed = $spreadsheetService->getListFeed($query);
+	$rowData = $listFeed->entries[0]->getCustom();
+
+	$result = array();
+
+	$data = $listFeed->entries[0]->getCustomByName("message");
+
+
+	foreach ($listFeed->entries as $line) {
+	  $status = $line->getCustomByName("status");
+	  if($status == DONE){
+	  	//skip
+	  	continue;
+	  }
+	  $result["message"] = (string)$line->getCustomByName("message");
+	  $result["theme"] = (string)$line->getCustomByName("theme");
+	  $result["status"] = "success";
+
+	  echo json_encode($result);
+	  //$newline = $line->getCustom();
+	  $newline["status"] = "DONE";
+	  //$spreadsheetService->deleteRow($listFeed->entries[0]);
+	  $spreadsheetService->updateRow($line,$newline);
+	  exit();
+	}
+	$result["status"] = "none";
+	echo json_encode($result);
+
+}catch(Exception $e){
+	echo $e->getMessage();
 }
-$result["status"] = "none";
-echo json_encode($result);
 
 
 /*
